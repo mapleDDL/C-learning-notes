0762d74d07b5ebda6bacd7d9a8fb180ef086e2fa
mapleDDL <zouzhiqun@zju.edu.cn>
2021-09-03 01:08:23 +0800

    2021.0903

---
+++ leetcode_cpp/jzoffer_22.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getKthFromEnd(ListNode* head, int k) {
+        auto res = head;
+        auto p = head;
+
+        for (int i = 0; i < k-1; i++) {
+            p = p->next;
+        }
+
+        while (p->next) {
+            res = res->next;
+            p = p->next;
+        }
+
+        return res;
+    }
+};
